369892108ca4e11eabcdcf57a25ce163929bc835
ayoisrael@outlook.com <ayoisrael@outlook.com>
2026-06-05 09:19:41 +0100

    team member update

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -9,7 +9,7 @@ const teamMembers = [
   },
   { name: "Erua Joseph Yusuf", github: "https://github.com/Eruxuf" },
   { name: "Okoro Oghenetega ", github: "https://github.com/INFlNlTEE" },
-  { name: "Placeholder Member 5", github: "https://github.com/placeholder" },
+  { name: "Oyesola Kenneth Oyedotun", github: "https://github.com/Kenoye001" },
   { name: "Placeholder Member 6", github: "https://github.com/placeholder" },
   { name: "Placeholder Member 7", github: "https://github.com/placeholder" },
   { name: "Placeholder Member 8", github: "https://github.com/placeholder" },
