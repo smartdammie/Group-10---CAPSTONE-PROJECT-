84a12984567eb5c8478f260da417339b0186ff8e
bukolaeg <bukolarongbe2015@gmail.com>
2026-06-06 13:58:51 +0100

    Added homepage styling

--- src/data/teamMembers.js
+++ src/data/teamMembers.js
@@ -10,7 +10,7 @@ const teamMembers = [
   { name: "Erua Joseph Yusuf", github: "https://github.com/Eruxuf" },
   { name: "Okoro Oghenetega ", github: "https://github.com/INFlNlTEE" },
   { name: "Kenneth Oyesola", github: "https://github.com/Kenoye001" },
-  { name: "Placeholder Member 6", github: "https://github.com/placeholder" },
+  { name: "Akinsipe Oluwabukola", github: "https://github.com/Bukolaeg" },
   { name: "Placeholder Member 7", github: "https://github.com/placeholder" },
   { name: "Placeholder Member 8", github: "https://github.com/placeholder" },
   { name: "Placeholder Member 9", github: "https://github.com/placeholder" },
